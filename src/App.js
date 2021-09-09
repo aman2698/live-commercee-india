dd3648b82ff029dfe21e9972f5b00e39cf1efb00
webrish <rishwebd@gmail.com>
2021-09-09 12:44:53 +0530

    add about page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import {useState,useEffect} from 'react'
-import './App.css';
-import {MemorizedFeature} from './components/Feature';
-import Header from './components/Header';
-import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
-import Services from './components/Services';
+import { useState, useEffect } from 'react'
+import './App.css'
+import { MemorizedFeature } from './components/Feature'
+import Header from './components/Header'
+import JsonData from './data/data.json'
+import SmoothScroll from 'smooth-scroll'
+import Services from './components/Services'
+import About from './components/About'
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
-}); 
+})
 
 function App() {
-  const [landingPageData, setLandingPageData] = useState({});
+  const [landingPageData, setLandingPageData] = useState({})
   useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+    setLandingPageData(JsonData)
+  }, [])
   return (
-    <div className="">
-     <Header/>
-     <Services data={landingPageData.Services}  />
-     <MemorizedFeature data={landingPageData.Features}/>
-    
+    <div className=''>
+      <Header />
+      <Services data={landingPageData.Services} />
+      <About data={landingPageData.About} />
+      <MemorizedFeature data={landingPageData.Features} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,38 @@
-const About = () => {
-  return <div></div>
+import Image from './../image/about.jpg'
+
+const About = ({ data }) => {
+  return (
+    <div id='about'>
+      <div
+        className='container mx-auto px-3 mt-5'
+        style={{ backgroundColor: '#f4f5f7' }}
+      >
+        <h1 className='text-center text-5xl font-bold'>About Us</h1>
+        <div className='grid grid-flow-col grid-cols-1 grid-rows-1 justify-items-center  mt-2'>
+          <span className='section-title ml-5'> </span>
+        </div>
+        <div className='grid-flow-col flex justify-space'>
+          <img src={Image} alt='' />
+
+          <div class='bg-green-500 rounded-lg m-4 h-12'>
+            <p>{data ? data.paragraph : 'loading...'}</p>
+            <br />
+            <br />
+            <h1 className='text-2xl font-bold'>Why Choose Us?</h1>
+            <div className='list-style'>
+              <div className='grid-col-6 gap-4'>
+                <ul>
+                  {data
+                    ? data.Why.map((d, i) => <li key={`${d}-${i}`}>{d}</li>)
+                    : 'loading'}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default About
